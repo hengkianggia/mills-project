3c60bfd3d467dfe0411af8f6d7dcc59496fda3b8
HengkiAnggia <pengkyjd00@gmail.com>
2023-08-18 10:33:47 +0700

    filter done

--- components/Indonesia collections/List.jsx
+++ components/Indonesia collections/List.jsx
@@ -1,23 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./list.module.css";
 import { MdFilterList } from "react-icons/md";
+import { MdFilterListOff } from "react-icons/md";
 import { AiOutlineDown } from "react-icons/ai";
+import { AiOutlineUp } from "react-icons/ai";
 import Items from "../Items/Items";
 
 const List = () => {
+  const [saringOpen, setSaringOpen] = useState(false);
+  const [urutkanOpen, setUrutkanOpen] = useState(false);
+
+  const saringHandler = () => {
+    setSaringOpen(!saringOpen);
+    setUrutkanOpen(false);
+  };
+
+  const urutkanHandler = () => {
+    setUrutkanOpen(!urutkanOpen);
+    setSaringOpen(false);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.topFilter}>
         <div className={styles.right}>
-          <span>
+          <span onClick={saringHandler}>
             <p>Saring</p>
-            <MdFilterList />
+            {saringOpen ? <MdFilterListOff /> : <MdFilterList />}
           </span>
 
-          <span>
+          <span onClick={urutkanHandler}>
             <p>Urutkan</p>
-            <AiOutlineDown />
+            {urutkanOpen ? <AiOutlineUp /> : <AiOutlineDown />}
           </span>
+
+          {saringOpen ? (
+            <div className={styles.filter}>
+              <ul>
+                <li>INDONESIA NATIONAL TEAM 2020</li>
+                <li>INDONESIA NATIONAL TEAM 2022</li>
+              </ul>
+            </div>
+          ) : (
+            ""
+          )}
+
+          {urutkanOpen ? (
+            <div className={styles.filter}>
+              <ul>
+                <li>Harga (Terendah ke Tertinggi)</li>
+                <li>Harga (Tertinggi ke Terendah)</li>
+                <li>Tanggal (Terbaru ke Lama)</li>
+              </ul>
+            </div>
+          ) : (
+            ""
+          )}
         </div>
       </div>
 
